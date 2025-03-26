d9c73cce90ff2c28777393fd0b5edfbac5359a81
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2025-03-26 15:30:54 +0530

    Add files via upload

--- sum.c
+++ sum.c
@@ -1,11 +1,28 @@
+/*program to calculate the sum of array elements by passing the array to a function.
+Display the sum in the main function.*/
 #include<stdio.h>
+int sum(int arr[],int limit){
+  int sum=0;
+  for(int i=0;i<limit;i++){
+    sum = sum + arr[i];
+  }
+  return sum;
+
+
+}
 
 int main(){
-  float number1,number2,sum;
-  printf("\n Enter two floating numbers");
-  scanf("%f%f",&number1,&number2);
-  sum=number1+number2;
-  printf("%f+%f=%f\n",number1,number2,sum);
+int limit;
+printf("Enter the limit:");
+scanf("%d",&limit);
+int arr[limit];
+printf("\n Enter the array of elements:");
+for(int i=0;i<limit;i++){
   
-  return 0;
+    scanf("%d",&arr[i]);
+  
 }
+printf("Sum of the Array is:%d ",sum(arr,limit));
+return 0;
+}
+
